4920dd2d826ebe225d3fcc9b4a27a8a097928eba
geekmdtravis <travis@geekmd.io>
2022-01-22 15:09:08 -0800

    fist goroutine

--- goroutines/main.go
+++ goroutines/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"sync"
+)
+
+var wg sync.WaitGroup
 
 func main() {
-	fmt.Println("Hello, playground")
+	sites := []string{"https://google.com", "https://facebook.com", "https://golang.org/"}
+
+	for _, site := range sites {
+		wg.Add(1)
+		go func(s string) {
+			res, err := http.Get(s)
+			if err != nil {
+				fmt.Println("Error:", err)
+			} else {
+				fmt.Println(s, ":", res.Status)
+			}
+			wg.Done()
+		}(site)
+	}
+
+	wg.Wait()
 }
